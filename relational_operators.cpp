00ada9698e7cf84f567dbf90111e16942f1717ac
sushanth426 <sushanth26malipati@gmail.com>
2020-03-22 12:56:00 -0400

    Update relational_operators.cpp

--- relational_operators.cpp
+++ relational_operators.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 	int n;
 	cin >> n;
 	for(int i = 0; i < n; i++){
-		long long m, k;
+		int m, k;
 		cin >> m >> k;
 		if(m < k) {cout << "<" << endl;}
 		else if(m > k) {cout << ">" << endl;}
